dbcbf6cc1d62fbb08fc804eb5224b3046c17a758
Gilbert Pellegrom <gilbert@pellegrom.me>
2016-09-26 14:23:21 +0100

    Include example run.sh

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# This is an example run.sh file that you
+# should include in the root of your project.
+
+# Start MySQL
+service mysql start
+
+# Output PHP version
+php -v
+
+# Run composer
+composer --help
